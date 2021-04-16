b6bca1963e0996a2a2bd42a982c0951090c6b401
dariodiaz <dario.dad@gmail.com>
2021-04-16 20:20:50 -0300

    Creating the constructor and visit method for the LoginPage class

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -4,4 +4,13 @@ import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
     WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public LoginPage visit() {
+        driver.get("https://www.saucedemo.com");
+        return this;
+    }
 }
