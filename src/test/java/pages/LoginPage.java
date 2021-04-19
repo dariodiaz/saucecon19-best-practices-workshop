9b067bc813b67eae48d70e7b69595634852eef2e
dariodiaz <dario.dad@gmail.com>
2021-04-19 00:19:56 -0300

    Basic structure for all page objects

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
@@ -12,5 +13,21 @@ public class LoginPage {
     public LoginPage visit() {
         driver.get("https://www.saucedemo.com");
         return this;
+    }
+
+    public InventoryPage login(String username, String password) {
+        String userField = "[data-test='username']";
+        String passField = "[data-test='password']";
+        String loginBtn = "#login-button";
+        // send username keystrokes
+        driver.findElement(By.cssSelector(userField)).sendKeys(username);
+
+        // send password keystrokes
+        driver.findElement(By.cssSelector(passField)).sendKeys(password);
+
+        // click login button to submit keystrokes
+        driver.findElement(By.cssSelector(loginBtn)).click();
+
+        return new InventoryPage(driver);
     }
 }
