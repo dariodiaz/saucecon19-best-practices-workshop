c995b8f07615f0221408f16a9f0d9e64c8974788
dariodiaz <dario.dad@gmail.com>
2021-04-20 17:37:48 -0300

    Removing other duplicated code

--- src/test/java/exercises/CheckoutFeatureTest.java
+++ src/test/java/exercises/CheckoutFeatureTest.java
@@ -1,51 +1,14 @@
 package exercises;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.MutableCapabilities;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.*;
-
-import java.lang.reflect.Method;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
-public class CheckoutFeatureTest {
-    protected WebDriver driver;
+public class CheckoutFeatureTest extends BaseTest {
 
-    @BeforeMethod
-    public void setUp(Method method) throws MalformedURLException
-    {
-        // Input your SauceLabs Credentials
-        String sauceUsername = System.getenv("SAUCE_USERNAME");
-        String sauceAccessKey = System.getenv("SAUCE_ACCESS_KEY");
-
-        MutableCapabilities capabilities = new MutableCapabilities();
-
-        //sets browser to Firefox
-        capabilities.setCapability("browserName", "firefox");
-
-        //sets operating system to macOS version 10.13
-        capabilities.setCapability("platform", "macOS 10.13");
-
-        //sets the browser version to 11.1
-        capabilities.setCapability("version", "58.0");
-
-        //sets your test case name so that it shows up in Sauce Labs
-        capabilities.setCapability("name", method.getName());
-        capabilities.setCapability("username", sauceUsername);
-        capabilities.setCapability("accessKey", sauceAccessKey);
-
-        //instantiates a remote WebDriver object with your desired capabilities
-        driver = new RemoteWebDriver(new URL("https://ondemand.saucelabs.com/wd/hub"), capabilities);
-    }
     @Test
     public void shouldBeAbleToCheckoutWithItems() {
         // wait 5 seconds
@@ -65,9 +28,4 @@ public class CheckoutFeatureTest {
         Assert.assertTrue(finalConfirmationPage.isLoaded());
     }
 
-    @AfterMethod
-    public void teardown(ITestResult result) {
-        ((JavascriptExecutor)driver).executeScript("sauce:job-result=" + (result.isSuccess() ? "passed" : "failed"));
-        driver.quit();
-    }
 }
